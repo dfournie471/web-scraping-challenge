c2fb3807316613f01a3a4a84852d6cea944add37
David Fournie <drfournie@gmail.com>
2021-02-09 21:48:26 -0600

    added complete skeleton files for assignment

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+def init_browser():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    return Browser("chrome", **executable_path, headless=True)
+
+def scrape_info():
+    url="https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    response=requests.get(url)
+    soup=bs(response.text,'lxml')
+    title = soup.find('div',class_="content_title").text
+    news_p = soup.find_all('div', class_="rollover_description_inner")[0].text
+
+
+    browser = init_browser()
+    url_2='https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url_2)
+    html_2=browser.html
+    soup_2=bs(html_2,"html.parser")
+    image_url=soup_2.find_all('img')[1]["src"]
+    featured_image='https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/' + image_url
+
+
+    url_3="https://space-facts.com/mars/"
+    tables=pd.read_html(url_3)
+    df=tables[0]
+    df.columns=['Category','Mars']
+    final_df=df.to_html(index=False)
+
+
+    hemisphere_image_urls=[]
+    click_lst=['Cerberus','Schiaparelli','Syrtis','Valles']
+    url_4='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    for x in range(4):
+        browser.visit(url_4)
+        browser.links.find_by_partial_text(click_lst[x]).click()
+        html_3=browser.html
+        soup_3=bs(html_3,'html.parser')
+        titles=soup_3.find('h2',class_="title").text
+        img_url=soup_3.find_all('img',class_="wide-image")[0]['src']
+        hemisphere_image_urls.append({"title":title,"img_url":img_url})
+    #close browser
+    browser.quit()
+
+    #save data scraped to dictionary
+    mars_data={
+        "headline_title":title,
+        "headline_par":news_p,
+        "featured_img_url":featured_image,
+        "df":final_df,
+        "image_titles":titles,
+        "hemisphere_pics":hemisphere_image_urls
+    }
+    
+    #return data
+    return mars_data
+
+
